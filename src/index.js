15eef1d4999de69795072820438f953566f4fa89
Pavan <pavanblackbuck10@gmail.com>
2026-02-04 19:12:24 +0530

    working on connecting to DB

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constant";
+
+const connectDB = async () => {
+    // try {
+    //     const 
+    // } catch (error) {
+    //     console.log("Error connecting to the database", error);
+    //     process.exit(1);
+    // }
+}
