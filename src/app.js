5d4b9182dbfc6c073ace3103e58eca86dedef314
P1Pavan <82015760+P1Pavan@users.noreply.github.com>
2026-02-12 14:37:15 +0530

    make route and controller

--- src/app.js
+++ src/app.js
@@ -13,4 +13,12 @@ app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// router imports
+
+import userRouter from  './routes/user.routes.js'
+
+// router declaration
+
+app.use("/api/v1/users",userRouter)
+
 export {app}
