d971cb3f337af656b395e055d34ae293b1563da5
P1Pavan <82015760+P1Pavan@users.noreply.github.com>
2026-02-18 02:20:38 +0530

    Add subscription model and format user routes for consistency

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controllers.js";
+import {
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+} from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -20,8 +25,8 @@ router.post(
   registerUser
 );
 
-router.route("/login").post(loginUser)
- // secured routes
- router.route("/logout").post(verifyJWT, logoutUser)
- router.route("/refresh-token").post(refreshAccessToken)
+router.route("/login").post(loginUser);
+// secured routes
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 export default router;
